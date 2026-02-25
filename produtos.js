80f4a28c44907a7b6ada84f2c07eff29044af6cc
lurdin17 <techguido17@gmail.com>
2026-02-25 02:20:43 -0300

    Adiciona menu mobile funcional em todas as páginas

--- produtos.js
+++ produtos.js
@@ -32,147 +32,154 @@ const BANCO_DE_DADOS = {
     "marca": "addiesdive"
     },
     "pg-pepsi": {
-    "nome": "PG GMT Pepsi",
+    "nome": " GMT Pepsi",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/o37Q581.jpeg", 
     "link": "marcas/pagani/pg_pepsi.html",
     "marca": "pagani" 
      },
      "pg-gmt-blue-sky": {
-    "nome": "PG GMT Blue Sky",
+    "nome": " GMT Blue Sky",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/QXbol96.jpeg", 
     "link": "marcas/pagani/pg_gmt_blue_sky.html",
     "marca": "pagani" 
      },
           "pg-gmt-red": {
-    "nome": "PG GMT Red",
+    "nome": " GMT Red",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/TOjG141.jpeg", 
     "link": "marcas/pagani/pg_gmt_red.html",
     "marca": "pagani" 
      },
                "pg-gmt-gray": {
-    "nome": "PG GMT Gray",
+    "nome": "GMT Gray",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/QKRLFaL.jpeg", 
     "link": "marcas/pagani/pg_gmt_gray.html",
     "marca": "pagani" 
      },
     "pg-gmt-brown": {
-    "nome": "PG GMT Brown",
+    "nome": "GMT Brown",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/SQElYEJ.jpeg",
     "link": "marcas/pagani/pg_gmt_brown.html",
     "marca": "pagani" 
      },
      "pg-bright-blue": {
-    "nome": "PG Bright Blue",
+    "nome": "PBright Blue",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/5suF4aT.jpeg",
     "link": "marcas/pagani/pg_bright_blue.html",
     "marca": "pagani"
      },
      "pg-wimbledon": {
-    "nome": "PG Wimbledon",
+    "nome": "PWimbledon",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/W2qBP2r.jpeg",
     "link": "marcas/pagani/pg_wimbledon.html",
     "marca": "pagani"
     },
+         "pg-olive-rose": {
+    "nome": "Olive Rosé",
+    "preco": 0.00,
+    "imagem": "https://i.imgur.com/lBiYjbs.jpeg",
+    "link": "marcas/pagani/pg_olive_rose.html",
+    "marca": "pagani"
+    },
     "pg-gmt-gold": {
-    "nome": "PG GMT Gold",
+    "nome": "PGMT Gold",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/ElbXqJt.jpeg",
     "link": "marcas/pagani/pg_gmt_gold.html",
     "marca": "pagani"
      },
          "pg-nautilus-moss": {
-    "nome": "PG Nautilus Moss",
+    "nome": "Nautilus Moss",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/wa9ohjj.jpeg",
     "link": "marcas/pagani/pg_nautilus_moss.html",
     "marca": "pagani"
      },
               "pg-nautilus-rose-b": {
-    "nome": "PG Nautilus Rosé B",
+    "nome": "Nautilus Rosé B",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/UvO7u71.jpeg",
     "link": "marcas/pagani/pg_nautilus_rose_b.html",
     "marca": "pagani"
      },
                    "pg-nautilus-white": {
-    "nome": "PG Nautilus White",
+    "nome": "Nautilus White",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/Omv9THG.jpeg",
     "link": "marcas/pagani/pg_nautilus_white.html",
     "marca": "pagani"
      },
             "pg-nautilus-bs": {
-    "nome": "PG Nautilus BS",
+    "nome": "Nautilus BS",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/z8JC9KE.jpeg",
     "link": "marcas/pagani/pg_nautilus_bs.html",
     "marca": "pagani"
      },
                 "pg-nautilus-rose-brown": {
-    "nome": "PG Nautilus Rosé Brown",
+    "nome": "Nautilus Rosé Brown",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/wXKX0cI.jpeg",
     "link": "marcas/pagani/pg_nautilus_rose_brown.html",
     "marca": "pagani"
      },
         "pg-rose-line": {
-    "nome": "PG Rosé Line",
+    "nome": "Rosé Line",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/2pqB5Z0.jpeg",
     "link": "marcas/pagani/pg_rose_line.html",
     "marca": "pagani"
      },
              "pg-rose-line-w": {
-    "nome": "PG Rosé Line W",
+    "nome": "Rosé Line W",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/EvjTfIK.jpeg",
     "link": "marcas/pagani/pg_rose_line_w.html",
     "marca": "pagani"
      },
              "pg-rose-line-dkb": {
-    "nome": "PG Rosé Line DKB",
+    "nome": "Rosé Line DKB",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/SXSiRlK.jpeg",
     "link": "marcas/pagani/pg_rose_line_dkb.html",
     "marca": "pagani"
      },
                  "pg-green": {
-    "nome": "PG Green",
+    "nome": "Green",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/pclH2f8.jpeg",
     "link": "marcas/pagani/pg_green.html",
     "marca": "pagani"
      },
                    "pg-dark-blue": {
-    "nome": "PG Dark Blue",
+    "nome": "Dark Blue",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/bF5MVFj.jpeg",
     "link": "marcas/pagani/pg_dark_blue.html",
     "marca": "pagani"
      },
                        "pg-black-rose": {
-    "nome": "PG Black Rosé",
+    "nome": "Black Rosé",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/cX9AzZe.jpeg",
     "link": "marcas/pagani/pg_black_rose.html",
     "marca": "pagani"
      },
                   "pg-rose-line-b": {
-    "nome": "PG Rosé Line B",
+    "nome": "Rosé Line B",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/oNrwx5q.jpeg",
     "link": "marcas/pagani/pg_rose_line_b.html",
     "marca": "pagani"
      },
               "pg-nautilus-dark-blue": {
-    "nome": "PG Nautilus Dark Blue",
+    "nome": "Nautilus Dark Blue",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/RpIc3Qd.jpeg",
     "link": "marcas/pagani/pg_nautilus_dark_blue.html",
@@ -200,7 +207,7 @@ const BANCO_DE_DADOS = {
     "marca": "holuns"
      },
      "pg-gmt-blue": {
-    "nome": "PG GMT Blue",
+    "nome": "GMT Blue",
     "preco": 0.00,
     "imagem": "https://i.imgur.com/vRKb8Pc.jpeg",
     "link": "marcas/pagani/pg_gmt_blue.html",
