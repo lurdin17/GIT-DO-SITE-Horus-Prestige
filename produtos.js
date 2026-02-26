76462818cbc2fe0c5e2553c4b3df10c4ecb9628d
lurdin17 <techguido17@gmail.com>
2026-02-26 03:01:10 -0300

    Adiciona menu mobile funcional em todas as páginas

--- produtos.js
+++ produtos.js
@@ -269,6 +269,13 @@ const BANCO_DE_DADOS = {
     "link": "marcas/holuns/ho_white_gold.html",
     "marca": "holuns"
      },
+                "ho-gold": {
+    "nome": "Gold",
+    "preco": 0.00,
+    "imagem": "https://i.imgur.com/35Fvq9x.jpeg",
+    "link": "marcas/holuns/ho_gold.html",
+    "marca": "holuns"
+     },
      "pg-gmt-blue": {
     "nome": "GMT Blue",
     "preco": 0.00,
