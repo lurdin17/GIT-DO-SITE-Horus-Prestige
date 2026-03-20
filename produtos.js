4c88d70cb2c4c084b93da08602e43fd05605f3e0
lurdin17 <techguido17@gmail.com>
2026-03-19 23:45:17 -0300

    Adiciona menu mobile funcional em todas as páginas

--- produtos.js
+++ produtos.js
@@ -7,222 +7,222 @@ const BANCO_DE_DADOS = {
     },
     "h3-silver": {
         nome: "Silver",
-        preco: 0.00,
+        preco: 579.99,
         imagem: "https://i.imgur.com/uY3FHOw.jpeg",
         link: "marcas/addiesdive/h3_silver.html"
     },
     "h3-black-silver": {
         nome: "Black Silver",
-        preco: 0.00,
+        preco: 579.99,
         imagem: "https://i.imgur.com/JzTWQDM.jpeg",
         link: "marcas/addiesdive/h3_black_silver.html"
     },
     "h3-blue": {
         nome: "Blue",
-        preco: 0.00,
+        preco: 579.99,
         imagem: "https://i.imgur.com/0FbpC0W.jpeg",
         link: "marcas/addiesdive/h3_blue.html",
         marca: "addiesdive"
     },
     "ad-pepsi-black": {
         nome: "Pepsi Black",
-        preco: 0.00,
+        preco: 809.99,
         imagem: "https://i.imgur.com/PrqRWAt.jpeg",
         link: "marcas/addiesdive/ad_pepsi_black.html",
         marca: "addiesdive"
     },
     "ad-pepsi-white": {
         nome: "Pepsi White",
-        preco: 0.00,
+        preco: 809.99,
         imagem: "https://i.imgur.com/T1lcO6L.jpeg",
         link: "marcas/addiesdive/ad_pepsi_white.html",
         marca: "addiesdive"
     },
     "h3-white": {
         nome: "White",
-        preco: 0.00,
+        preco: 579.99,
         imagem: "https://i.imgur.com/4btqUUA.jpeg",
         link: "marcas/addiesdive/h3_white.html",
         marca: "addiesdive"
     },
     "h3-green": {
         nome: "Green",
-        preco: 0.00,
+        preco: 579.99,
         imagem: "https://i.imgur.com/ALlI1EJ.jpeg",
         link: "marcas/addiesdive/h3_green.html",
         marca: "addiesdive"
     },
     "h3-zenturion-green": {
         nome: "Zenturion Green",
-        preco: 0.00,
+        preco: 579.99,
         imagem: "https://i.imgur.com/tp0iKgm.jpeg",
         link: "marcas/addiesdive/h3_zenturion_green.html",
         marca: "addiesdive"
     },
     "pg-pepsi": {
         nome: "GMT Pepsi",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/o37Q581.jpeg",
         link: "marcas/pagani/pg_pepsi.html",
         marca: "pagani"
     },
     "pg-gmt-blue-sky": {
         nome: "GMT Blue Sky",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/QXbol96.jpeg",
         link: "marcas/pagani/pg_gmt_blue_sky.html",
         marca: "pagani"
     },
     "pg-gmt-red": {
         nome: "GMT Red",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/TOjG141.jpeg",
         link: "marcas/pagani/pg_gmt_red.html",
         marca: "pagani"
     },
     "pg-gmt-gray": {
         nome: "GMT Gray",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/QKRLFaL.jpeg",
         link: "marcas/pagani/pg_gmt_gray.html",
         marca: "pagani"
     },
     "pg-gmt-brown": {
         nome: "GMT Brown",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/SQElYEJ.jpeg",
         link: "marcas/pagani/pg_gmt_brown.html",
         marca: "pagani"
     },
     "pg-bright-blue": {
         nome: "Bright Blue",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/5suF4aT.jpeg",
         link: "marcas/pagani/pg_bright_blue.html",
         marca: "pagani"
     },
     "pg-wimbledon": {
         nome: "Wimbledon",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/W2qBP2r.jpeg",
         link: "marcas/pagani/pg_wimbledon.html",
         marca: "pagani"
     },
     "pg-white-texture": {
         nome: "White Texture",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/NLww8GZ.jpeg",
         link: "marcas/pagani/pg_white_texture.html",
         marca: "pagani"
     },
     "pg-olive-rose": {
         nome: "Olive Rosé",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/lBiYjbs.jpeg",
         link: "marcas/pagani/pg_olive_rose.html",
         marca: "pagani"
     },
     "pg-gmt-gold": {
         nome: "GMT Gold",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/ElbXqJt.jpeg",
         link: "marcas/pagani/pg_gmt_gold.html",
         marca: "pagani"
     },
     "pg-nautilus-moss": {
         nome: "Nautilus Moss",
-        preco: 0.00,
+        preco: 1049.99,
         imagem: "https://i.imgur.com/wa9ohjj.jpeg",
         link: "marcas/pagani/pg_nautilus_moss.html",
         marca: "pagani"
     },
     "pg-nautilus-rose-b": {
         nome: "Nautilus Rosé B",
-        preco: 0.00,
+        preco: 1049.99,
         imagem: "https://i.imgur.com/UvO7u71.jpeg",
         link: "marcas/pagani/pg_nautilus_rose_b.html",
         marca: "pagani"
     },
     "pg-nautilus-white": {
         nome: "Nautilus White",
-        preco: 0.00,
+        preco: 1049.99,
         imagem: "https://i.imgur.com/Omv9THG.jpeg",
         link: "marcas/pagani/pg_nautilus_white.html",
         marca: "pagani"
     },
     "pg-nautilus-bs": {
         nome: "Nautilus BS",
-        preco: 0.00,
+        preco: 1049.99,
         imagem: "https://i.imgur.com/z8JC9KE.jpeg",
         link: "marcas/pagani/pg_nautilus_bs.html",
         marca: "pagani"
     },
     "pg-nautilus-rose-brown": {
         nome: "Nautilus Rosé Brown",
-        preco: 0.00,
+        preco: 1049.99,
         imagem: "https://i.imgur.com/wXKX0cI.jpeg",
         link: "marcas/pagani/pg_nautilus_rose_brown.html",
         marca: "pagani"
     },
     "pg-rose-line": {
         nome: "Rosé Line",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/2pqB5Z0.jpeg",
         link: "marcas/pagani/pg_rose_line.html",
         marca: "pagani"
     },
     "pg-rose-line-w": {
         nome: "Rosé Line W",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/EvjTfIK.jpeg",
         link: "marcas/pagani/pg_rose_line_w.html",
         marca: "pagani"
     },
     "pg-rose-line-dkb": {
         nome: "Rosé Line DKB",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/SXSiRlK.jpeg",
         link: "marcas/pagani/pg_rose_line_dkb.html",
         marca: "pagani"
     },
     "pg-green": {
         nome: "Green",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/pclH2f8.jpeg",
         link: "marcas/pagani/pg_green.html",
         marca: "pagani"
     },
     "pg-dark-blue": {
         nome: "Dark Blue",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/bF5MVFj.jpeg",
         link: "marcas/pagani/pg_dark_blue.html",
         marca: "pagani"
     },
     "pg-black-rose": {
         nome: "Black Rosé",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/cX9AzZe.jpeg",
         link: "marcas/pagani/pg_black_rose.html",
         marca: "pagani"
     },
     "pg-rose-line-b": {
         nome: "Rosé Line B",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/oNrwx5q.jpeg",
         link: "marcas/pagani/pg_rose_line_b.html",
         marca: "pagani"
     },
     "pg-nautilus-dark-blue": {
         nome: "Nautilus Dark Blue",
-        preco: 0.00,
+        preco: 1049.99,
         imagem: "https://i.imgur.com/RpIc3Qd.jpeg",
         link: "marcas/pagani/pg_nautilus_dark_blue.html",
         marca: "pagani"
     },
     "pg-gmt-blue": {
         nome: "GMT Blue",
-        preco: 0.00,
+        preco: 1089.99,
         imagem: "https://i.imgur.com/vRKb8Pc.jpeg",
         link: "marcas/pagani/pg_gmt_blue.html",
         marca: "pagani"
